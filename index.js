2b3c84bf094dcdeb4c9ae869f79f7ad577822cf7
Nicole <nicole.dupre@gmail.com>
2021-07-21 17:00:08 -0400

    new repo from scratch
    due to conflicts with branches

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.nav-icon4').click(function(){
+		$(this).toggleClass('open');
+		$('.container-mobile').toggleClass('open');
+	});
+
+	
+});
+
+
+
+console.log("I'm working");
