10ac7728664c5e742bebe7ccaeef65632480e300
Nicole <nicole.dupre@gmail.com>
2021-07-22 19:31:04 -0400

    my work
    content added

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 	$('.nav-icon4').click(function(){
 		$(this).toggleClass('open');
 		$('.container-mobile').toggleClass('open');
-	});
-
+		$('body').toggleClass('no-scroll');
+	});	
 	
 });
 
