6589015db515d1f8bdc78fcff5947c63f80820fb
Nicole <nicole.dupre@gmail.com>
2021-07-25 19:52:34 -0400

    my work-project section
    hero section changes and adjustments

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ $(document).ready(function(){
 	
 });
 
+let mainNavLinks = document.querySelectorAll("nav.in-page ul li a");
+let mainSections = document.querySelectorAll("wrapper section");
 
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
 
 console.log("I'm working");
